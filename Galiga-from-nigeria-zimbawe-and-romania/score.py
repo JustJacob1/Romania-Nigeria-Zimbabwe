6a8b4d5ca5371febf129c6aee77f54a6a2a955f7
JustJacob1 <jacoble-coderschoolpasadena@outlook.com>
2025-03-22 13:59:58 -0700

    Update score.py

--- Galiga-from-nigeria-zimbawe-and-romania/score.py
+++ Galiga-from-nigeria-zimbawe-and-romania/score.py
@@ -5,8 +5,8 @@ class Score:
     def read_file(self):
         file = "Galiga-from-nigeria-zimbawe-and-romania\highscore.txt"
         open_file = open(file)
-        print(open_file.read())
-        open_file.close()
+        return int(open_file.read())
+        
 
     def write_file(self, score):
         file =  open("Galiga-from-nigeria-zimbawe-and-romania\highscore.txt", "w")
@@ -15,5 +15,5 @@ class Score:
 
     def high_score(self, points, high_score):
         if points > high_score:
-            self.write_file(points)
+            self.write_file(str(points))
         
